ab184376b732b361e50244f384a82ef3eebf2c45
Mark Robert Johnson <MarkJohnson1974@live.com>
2013-06-20 01:28:21 -0700

    Initial checkin

---
+++ Get_NugetPackages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+
+using NuGet;
+
+namespace NugetExplorer
+{
+	[Cmdlet(VerbsCommon.Get,"NugetPackages")]
+	
+    public class Get_NugetPackages : PSCmdlet
+    {
+		[Parameter(Position = 0, Mandatory = false, HelpMessage = "The URI of the Nuget package repository")]
+		public string Source { get; set; }
+
+		protected  override  void BeginProcessing()
+		{
+			var repo = PackageRepositoryFactory.Default.CreateRepository(Source);
+			
+			WriteObject(repo.GetPackages());
+		}
+    }
+}
